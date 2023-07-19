766c8db8b09681218488a54b1349449308886ea2
alvito <alvitozidane76@gmail.com>
2023-07-19 19:38:42 +0700

    susah

--- getData.py
+++ getData.py
@@ -1,14 +1,45 @@
 import sys
 import numpy
 import quandl
+import pandas
 
-numpy.set_printoptions(threshold=sys.maxsize)
+import re
+
 
 df = quandl.get("WIKI/GOOGL")
-# print(df.head())
+# df.set_printoptions(threshold=sys.maxsize)
+
+
+def print_full(x):
+    pandas.set_option('display.max_rows', None)
+    pandas.set_option('display.max_columns', None)
+    pandas.set_option('display.width', 2000)
+    pandas.set_option('display.float_format', '{:20,.2f}'.format)
+    pandas.set_option('display.max_colwidth', None)
+    return x
+    # pandas.reset_option('display.max_rows')
+    # pandas.reset_option('display.max_columns')
+    # pandas.reset_option('display.width')
+    # pandas.reset_option('display.float_format')
+    # pandas.reset_option('display.max_colwidth')
+
+
+# numpy.set_printoptions(threshold=numpy.inf)
+# print(print_full(df))
 
 # c = [i for i in range(100)]
 
-f = open("stocking-google.txt", "a")
-f.write(str(df))
+
+s = open("stocking-google.txt", "r")
+c = re.sub("\s+", ",", s.read().strip())
+# print(c)
+
+f = open("stocking-google2.csv", "a")
+f.write(str(c))
 f.close()
+
+# s.close()
+
+# df = pandas.read_csv("stocking-google2.csv")
+
+# print(df.head())
